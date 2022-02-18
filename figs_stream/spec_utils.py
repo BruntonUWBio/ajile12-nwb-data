dcb96784e13996832a1e173f44bcbd117788ecd0
Steven Peterson <stepeter@umich.edu>
2022-02-17 19:28:12 -0800

    Add comments and header

--- figs_stream/spec_utils.py
+++ figs_stream/spec_utils.py
@@ -1,3 +1,43 @@
+"""Utility functions for computing ECoG spectral power.
+
+Subfunctions used by compute_cont_spec.py to compute spectral
+power using Welch's method and project spectral power from
+ECoG electrodes onto common brain regions of interest.
+Use when streaming data directly from DANDI instead of
+running on local NWB files.
+
+
+Author
+------
+Steven Peterson
+
+
+Modification history
+--------------------
+02/17/2022 - Add comments and header
+07/16/2021 - Created script
+
+
+License
+-------
+Copyright (c) 2020 CatalystNeuro
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import numpy as np
 from scipy.io import loadmat
 
